25788dcedd7c39f17ba4d63541b0a1efa73d6fe7
Mr_JiaoMin <vip_jiaomin@163.com>
2016-12-05 16:06:41 +0800

    1.MyApplication 完善

--- app/src/main/java/com/example/jiaomin/beautypa/app/MyApplication.java
+++ app/src/main/java/com/example/jiaomin/beautypa/app/MyApplication.java
@@ -2,14 +2,23 @@ package com.example.jiaomin.beautypa.app;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.DisplayMetrics;
+
+import com.example.jiaomin.beautypa.utils.LogUtil;
+
+import java.util.Locale;
 
 /**
  * Created by MrJiaoMin@outlook.com on 2016/12/1.
  * 类描述：自定义的 AppLication
  */
 
-public class MyApplication extends Application{
-    private static MyApplication mInstance ;
+public class MyApplication extends Application {
+    private static MyApplication mInstance;
     private static Context mContext;
     /**
      * 屏幕宽度
@@ -22,7 +31,7 @@ public class MyApplication extends Application{
     /**
      * 屏幕密度
      */
-    public int screenDensity;
+    public float screenDensity;
 
 
     @Override
@@ -31,6 +40,70 @@ public class MyApplication extends Application{
         mContext = getApplicationContext();
         mInstance = this;
 
+//      初始化日志操作工具类  开启日志打印 ， 关闭日志写入文件功能 ， 日志打印类型为 d ， 日志tag 为 MrJiaoMin
+        LogUtil.init(this, true, false, 'd', "MrJiaoMin");
+    }
 
+
+    public static Context getInstance() {
+        return mInstance;
+    }
+
+    /**
+     * 获取版本号
+     *
+     * @return 当前应用的版本号
+     */
+    public static String getVersion() {
+        PackageManager packageManager = mInstance.getPackageManager();
+        String version;
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(mInstance.getPackageName(), 0);
+            version = packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            version = "";
+        }
+
+        LogUtil.e("version" + version);
+        return version;
+    }
+
+    /**
+     * 获取当前系统语言
+     *
+     * @return
+     */
+    public static String getLanguage() {
+        Locale locale = Locale.getDefault();
+        String language = locale.getDefault().toString();
+        return language;
+    }
+
+    /**
+     * 获取当前屏幕的宽高
+     */
+    public void initScreenSize() {
+        DisplayMetrics currMetrics = getApplicationContext().getResources().getDisplayMetrics();
+        screenHeight = currMetrics.heightPixels;
+        screenWidth = currMetrics.widthPixels;
+        screenDensity = currMetrics.density;
+    }
+
+    /**
+     * 判断网络是否有连接
+     *
+     * @param context
+     * @return true 正常连接中
+     */
+    public static boolean isNetwokrkAvailable(Context context) {
+        if (context != null) {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+            if (networkInfo != null) {
+                return networkInfo.isAvailable();
+            }
+        }
+        return false;
     }
 }
